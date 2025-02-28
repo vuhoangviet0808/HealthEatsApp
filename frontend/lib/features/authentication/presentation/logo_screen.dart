2c4dbb85e43a3122b504e4621ec8e3b3662989fa
unknown <vuotngokhong@gmail.com>
2025-02-28 23:05:16 +0700

    Thanh cong cau hinh api cho login va register

--- frontend/lib/features/authentication/presentation/logo_screen.dart
+++ frontend/lib/features/authentication/presentation/logo_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../../core/themes/theme.dart';
 
 class LogoScreen extends StatefulWidget {
   @override
